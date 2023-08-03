a3de12a1bd62cda5895fa990661ae0aac88fabb3
shlegel <sveta.shlegel@mail.ru>
2023-08-03 09:25:10 +0300

    added product view

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.views import home, contacts, ProductView, create
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts),
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('<int:pk>', ProductView.as_view(), name='product-detail'),
+    path('create', create, name='create'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from catalog.models import Product
+from django.views.generic import DetailView
+
+
+class ProductView(DetailView):
+    model = Product
+    template_name = 'catalog/product_view.html'
+    context_object_name = 'product'
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+
+    product_list = Product.objects.all()
+    content = {
+        'object_list': product_list
+    }
+    return render(request, 'catalog/home.html', content)
 
 
 def contacts(request):
@@ -12,3 +25,22 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'{name} ({phone}): {message}')
     return render(request, 'catalog/contacts.html')
+
+
+def create(request):
+    product_for_create = []
+    if request.method == 'POST':
+        product = {
+            'name': request.POST.get('name'),
+            'description': request.POST.get('description'),
+            'category': request.POST.get('category'),
+            'price': request.POST.get('price'),
+            'creation_date': request.POST.get('creation_date'),
+            'last_change_date': request.POST.get('last_change_date')
+        }
+        product_for_create.append(
+            Product(**product)
+        )
+        Product.objects.bulk_create(product_for_create)
+
+    return render(request, 'catalog/create.html')
